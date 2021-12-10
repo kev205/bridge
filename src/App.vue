1c8d1cebf75bcc5a971111bcceaacbe706077d4b
kevin <ulrichkev2017@gmail.com>
2021-12-10 01:18:11 +0100

    v005

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="App" data-app="app">
     <!-- Navigation -->
-    <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+    <nav v-if="isLoggedIn" class="container navbar navbar-expand-lg navbar-light bg-light">
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <div class="navbar-nav mr-auto">
           <router-link to="/products" class="nav-item nav-link"
